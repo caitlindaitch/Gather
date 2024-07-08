32dfcda07829cc6bba05cdf84b86de7dff6680b0
Caitlin Daitch <caitlindaitch@Caitlins-MacBook-Air.local>
2024-07-08 19:54:55 -0400

    Initial project setup

---
+++ Gather/GatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  GatherApp.swift
+//  Gather
+//
+//  Created by Caitlin Daitch on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct GatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
