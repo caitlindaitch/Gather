e090c08a10b4e655e975a353fd4f691a16e31c6e
Caitlin Daitch <caitlindaitch@Caitlins-MacBook-Air.local>
2024-11-21 11:26:15 -0500

    Simplify data binding logic

--- Gather/ContentView.swift
+++ Gather/ContentView.swift
@@ -1,19 +1,19 @@
 import SwiftUI
 
+
 struct AppView: View {
     @StateObject var flowViewModel: AppFlowViewModel
-    var createEventViewModel: CreateEventViewModel
 
     var body: some View {
         NavigationStack(path: $flowViewModel.navigationPath) {
             GatherLandingView(viewModel: flowViewModel)
             .navigationDestination(for: Int.self) { i in
-                CreateEventView(flowViewModel: flowViewModel, createEventViewModel: createEventViewModel)
+                CreateEventScreen(flowViewModel: flowViewModel)
             }
         }
     }
 }
 
 #Preview {
-    AppView(flowViewModel: AppFlowViewModel(), createEventViewModel: CreateEventViewModel(model: CreateEventModel(), defaultsManager: DefaultsManager()))
+    AppView(flowViewModel: AppFlowViewModel())
 }
--- Gather/CreateEvent/CreateEventView.swift
+++ Gather/CreateEvent/CreateEventView.swift
@@ -1,37 +1,46 @@
 import PhotosUI
 import SwiftUI
 
-struct CreateEventView: View {
+struct Event {
+    let name: String
+    let location: String
+    let date: Date
+    let eventDescription: String
+}
+
+struct CreateEventScreen: View {
     @StateObject var flowViewModel: AppFlowViewModel
-    var createEventViewModel: CreateEventViewModel
+    @State var name: String = ""
+    @State var location: String = ""
+    @State var eventDate: Date = Date()
+    @State var eventDescription: String = ""
 
     var body: some View {
         Form {
             Section {
-                TextField("Name", text: createEventViewModel.$name)
-                TextField("Location", text: createEventViewModel.$location)
-                PhotosPicker("Upload image", selection: createEventViewModel.$image, matching: .images)
-                DatePicker("Date", selection: createEventViewModel.$date)
+                TextField("Name", text: $name)
+                TextField("Location", text: $location)
+                DatePicker("Date", selection: $eventDate)
             } header: {
                 Text("Event details")
             }
 
             Section {
-                TextEditor(text: createEventViewModel.$description)
+                TextEditor(text: $eventDescription)
                     .frame(height: 200)
             } header: {
                 Text("Description")
             }
 
             Section {
-                Button(action: createEventViewModel.saveEvent) {
-                    Text("Save event")
-                }
+//                Button(action: createEventViewModel.saveEvent) {
+//                    Text("Save event")
+//                }
             }
         }
     }
 }
 
-#Preview {
-    CreateEventView(flowViewModel: AppFlowViewModel(), createEventViewModel: CreateEventViewModel(model: CreateEventModel(), defaultsManager: DefaultsManager()))
-}
+//#Preview {
+//    CreateEventView(flowViewModel: AppFlowViewModel())
+//}
